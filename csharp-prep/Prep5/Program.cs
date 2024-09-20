1d6816eda5979e56856108c88476fee6d834d9da
Wilson Houston <wilsonhouston@Wilsons-MacBook-Air.local>
2024-09-20 13:28:57 -0600

    Updated Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Globalization;
+using System.Numerics;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        int squaredNumber = 0;
+        // function that display message returns nothing
+        void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the program");
+        }
+
+        // function that request name then returns name
+        string PromtUserName()
+        {
+            Console.Write("What's you name? ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        int PromtUserNumber()
+        {
+            Console.Write("What's your favorite number? ");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+
+        double Computate(double number)
+        {
+            squaredNumber = (int)Math.Pow(number, 2);
+            return squaredNumber;
+        }
+
+        void DisplayResult(string name,double squared)
+        {
+            Console.WriteLine($"Hello {name}. The square of your number is {squared}");
+        }
+
+        double Squared = Computate(PromtUserNumber());
+
+        DisplayResult(PromtUserName(), Squared);
+
     }
+
+
 }
