5fbcdf771f7bc99c00add81fc3bebaef2a8ca45f
Wilson Houston <wilsonhouston@Wilsons-MacBook-Air.local>
2024-09-19 22:21:44 -0600

    Updated prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Data;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // call Radom call
+        Random randomGen = new Random();
+
+        // called the randomGen with the Next method that takes in two parmeters
+        // Created a variable that's equals the number generator 
+        int randomNum = randomGen.Next(1,10);
+
+        Console.Write("Enter a number between 1-10: ");
+        int numInput = int.Parse(Console.ReadLine());
+        int attempts = 0;
+
+        while (numInput != randomNum)
+        {
+            Console.Clear();
+            attempts ++;
+            if (numInput < randomNum)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Lower");
+            }
+
+            numInput = int.Parse(Console.ReadLine());
+
+        } 
+
+        Console.WriteLine("You did it, great job!!");
+        Console.WriteLine($"Attempts: {attempts}");
+
     }
 }
