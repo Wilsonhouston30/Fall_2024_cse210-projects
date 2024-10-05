7b9c65951f6eab0a7a873f7180f4702209f47fd6
Wilson Houston <wilsonhouston@Wilsons-MacBook-Air.local>
2024-10-05 00:13:58 -0600

    Main program // Created menu for user

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Xml;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        static void DisplayMenu()
+        {
+            string [] options = {"New Entry", "Load Entry", "Save Entry", "Quit" };
+            
+
+            for (int i = 0; i < options.Length; i++ )
+            {
+                Console.WriteLine($"{i}: {options[i]}");
+            }
+        }
+
+        Console.Clear();
+        DisplayMenu();
+
+
+
+
+        
     }
 }
